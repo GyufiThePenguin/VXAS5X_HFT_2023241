b64bccd6225ba1be7734551ece9731bc7b26c76c
GyufiThePenguin <tothzsombor200101@gmail.com>
2023-10-21 12:10:19 +0200

    Created project structures

---
+++ VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VXAS5X_HFT_2023241.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VXAS5X_HFT_2023241.Test
+{
+    public class Class1
+    {
+    }
+}
