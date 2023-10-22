0bfd146b9eebb6d8845218f88c5c981b74d84017
GyufiThePenguin <tothzsombor200101@gmail.com>
2023-10-22 16:28:18 +0200

    Added basic functionality to StagePlay.cs

--- VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Models/StagePlay.cs
+++ VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Models/StagePlay.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VXAS5X_HFT_2023241.Models
 {
-    internal class StagePlay
+    public class StagePlay
     {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public int Premier { get; set; }
+        public int Profit { get; set; }
+        public string Rating { get; set; }
+
+        [ForeignKey(nameof(Dramaturg))]
+        public int DramaturgId { get; set; }
+
+        [NotMapped]
+        public virtual Dramaturg Dramaturg { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Actor> Actors { get; set; }
+
+        public StagePlay()
+        {
+            Actors = new HashSet<Actor>();
+        }
+
     }
 }
