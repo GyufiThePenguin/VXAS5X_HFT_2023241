b7aa91a8ec4df020d3d8ee50ec9690c9f0236201
GyufiThePenguin <tothzsombor200101@gmail.com>
2023-10-22 16:27:07 +0200

    Added basic functionality to Actor.cs

--- VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Models/Actor.cs
+++ VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Models/Actor.cs
@@ -1,8 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 
 namespace VXAS5X_HFT_2023241.Models
 {
     public class Actor
     {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Role { get; set; }
+
+
+        [NotMapped]
+        [JsonIgnore]
+        public virtual ICollection<StagePlay> Plays { get; set; }
+
+
+        public Actor()
+        {
+            Plays = new HashSet<StagePlay>() { }
+            ;
+
+        }
+
     }
 }
