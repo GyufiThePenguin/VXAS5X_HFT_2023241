6104ed0637cc9165dfb7506d655a1cf956ec18fa
GyufiThePenguin <tothzsombor200101@gmail.com>
2023-10-24 16:36:21 +0200

    Created IStagePLayRepo.cs

---
+++ VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Repository/IStagePlayRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VXAS5X_HFT_2023241.Models;
+
+namespace VXAS5X_HFT_2023241.Repository
+{
+    public interface IStagePlayRepo
+    {
+        void Create(StagePlay stagePlay);
+        void Update(StagePlay stageplay);
+        void Delete(int id);
+
+        StagePlay Read(int id);
+
+        IQueryable<StagePlay> ReadAll();
+
+
+    }
+}
