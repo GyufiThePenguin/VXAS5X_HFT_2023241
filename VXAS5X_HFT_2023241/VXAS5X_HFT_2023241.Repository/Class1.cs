13fa7a4cd46a8ed5d64a635e3eab888a278c2f46
GyufiThePenguin <tothzsombor200101@gmail.com>
2023-10-24 16:34:22 +0200

    Created IActorRepo.cs

--- VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace VXAS5X_HFT_2023241.Repository
-{
-    public class Class1
-    {
-    }
-}
---
+++ VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Repository/IActorRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VXAS5X_HFT_2023241.Models;
+
+namespace VXAS5X_HFT_2023241.Repository
+{
+    public interface IActorRepo
+    {
+
+        void Create(Actor actor);
+        void Update(Actor actor);
+        void Delete(int id);
+
+        Actor Read(int id);
+
+        IQueryable<Actor> ReadAll();
+
+
+    }
+}
