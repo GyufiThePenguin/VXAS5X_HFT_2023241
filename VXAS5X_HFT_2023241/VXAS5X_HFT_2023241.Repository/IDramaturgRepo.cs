bd6c288c796598d5d7d419db6ca7397add62312f
GyufiThePenguin <tothzsombor200101@gmail.com>
2023-10-24 16:35:31 +0200

    Created IDramaturgRepo.cs

---
+++ VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Repository/IDramaturgRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VXAS5X_HFT_2023241.Models;
+
+namespace VXAS5X_HFT_2023241.Repository
+{
+    public interface IDramaturgRepo
+    {
+
+        void Create(Dramaturg dramaturg);
+        void Update(Dramaturg dramaturg);
+        void Delete(int id);
+
+        Dramaturg Read(int id);
+
+        IQueryable<Dramaturg> ReadAll();
+
+
+    }
+}
