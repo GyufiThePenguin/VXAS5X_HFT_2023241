32a9fe581f619b9182f107baad4f376cc02dc4ed
GyufiThePenguin <tothzsombor200101@gmail.com>
2023-10-25 14:53:54 +0200

    Created DramaturgLogic class with robust CRUD methods and exception handling.

---
+++ VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Logic/DramaturgLogic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VXAS5X_HFT_2023241.Models;
+using VXAS5X_HFT_2023241.Repository;
+
+namespace VXAS5X_HFT_2023241.Logic
+{
+    public class DramaturgLogic : IDramaaturgLogic
+    {
+
+        IDramaturgRepo dramaturgRepo;
+
+        public DramaturgLogic(IDramaturgRepo dramaturgRepo)
+        {
+            this.dramaturgRepo = dramaturgRepo;
+
+        }
+
+        public void Create(Dramaturg dramaturg)
+        {
+            if (dramaturg.Name != null)
+                dramaturgRepo.Create(dramaturg);
+            else
+                throw new ArgumentException("ERROR: You royally fcked up!");
+
+        }
+
+        public Dramaturg Read(int id)
+        {
+            if (dramaturgRepo.ReadAll().Any(x => x.Id.Equals(id)))
+                return dramaturgRepo.Read(id);
+
+            throw new IndexOutOfRangeException("ERROR: Can not read -> wrong ID.");
+
+        }
+
+        public void Delete(int id)
+        {
+            if (dramaturgRepo.ReadAll().Any(x => x.Id.Equals(id)))
+                dramaturgRepo.Delete(id);
+            else
+                throw new IndexOutOfRangeException("ERROR: Can not delete -> wrong ID.");
+
+        }
+
+        public void Update(Dramaturg dramaturg)
+        {
+            if (dramaturgRepo.ReadAll().Any(x => x.Id.Equals(dramaturg.Id)))
+                dramaturgRepo.Update(dramaturg);
+            else
+                throw new IndexOutOfRangeException("ERROR: Can not update -> wrong ID.");
+
+        }
+
+        public IEnumerable<Dramaturg> ReadAll()
+        {
+            return dramaturgRepo.ReadAll();
+
+        }
+
+    }
+}
