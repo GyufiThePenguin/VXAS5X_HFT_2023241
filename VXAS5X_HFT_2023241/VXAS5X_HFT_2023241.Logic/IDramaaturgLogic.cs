8798e74b2ae9ede5f2f160c5756cb1ef236fed15
GyufiThePenguin <tothzsombor200101@gmail.com>
2023-10-25 14:11:58 +0200

    Created the neccessary intrerfaces

---
+++ VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Logic/IDramaaturgLogic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VXAS5X_HFT_2023241.Models;
+
+namespace VXAS5X_HFT_2023241.Logic
+{
+    public interface IDramaaturgLogic
+    {
+        void Create(Dramaturg dramaturg);
+        void Delete(int id);
+        void Update(Dramaturg dramaturg);
+
+        Dramaturg Read(int id);
+
+        IEnumerable<Dramaturg> ReadAll();
+
+
+    }
+}
---
+++ VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Logic/IStagePlayLogic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VXAS5X_HFT_2023241.Models;
+
+namespace VXAS5X_HFT_2023241.Logic
+{
+    public interface IStagePlayLogic
+    {
+        void Create(StagePlay stagePlay);
+        void Update(StagePlay stageplay);
+        void Delete(int id);
+        double AvgProfit();
+        int GoodPlays();
+
+        StagePlay Read(int id);
+
+        IEnumerable<StagePlay> HorribleSuccessStagePlays();
+        IEnumerable<StagePlay> ExceptionalStagePlays();
+
+        IEnumerable<StagePlay> NotHorribleTuktukStagePlays();
+        IEnumerable<StagePlay> RegisseurBelaBalla();
+        IEnumerable<StagePlay> PremieredAfter2010();
+
+        IEnumerable<StagePlay> ReadAll();
+
+
+
+    }
+}
