c16b6c34edf809c07ecc4ce96a81842792d7fe7d
GyufiThePenguin <tothzsombor200101@gmail.com>
2023-11-03 13:37:20 +0100

    Code cleanup

--- VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Endpoint/Controllers/ActorController.cs
+++ VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Endpoint/Controllers/ActorController.cs
@@ -7,9 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using VXAS5X_HFT_2023241.Endpoint.Services;
-using VXAS5X_HFT_2023241.Logic;
-using VXAS5X_HFT_2023241.Models;
 
 
 namespace VXAS5X_HFT_2023241.Endpoint.Controllers
