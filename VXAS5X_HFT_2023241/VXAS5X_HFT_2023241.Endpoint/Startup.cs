755d06a7f341fb2723da10210d3a258ab765bdf4
GyufiThePenguin <tothzsombor200101@gmail.com>
2023-11-27 13:51:05 +0100

    Buf fixes

--- VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Endpoint/Startup.cs
+++ VXAS5X_HFT_2023241/VXAS5X_HFT_2023241.Endpoint/Startup.cs
@@ -7,6 +7,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VXAS5X_HFT_2023241.Endpoint.Services;
+using VXAS5X_HFT_2023241.Logic;
+using VXAS5X_HFT_2023241.Repository;
+using VXAS5X_HFT_2023241.Endpoint.Services;
 using VXAS5X_HFT_2023241.Logic;
 using VXAS5X_HFT_2023241.Repository;
 
