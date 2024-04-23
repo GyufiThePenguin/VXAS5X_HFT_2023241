f2ba1205ea78bb39f075ad90e74200253d911fea
Zsombor Tóth <tothzsombor200101@gmail.com>
2024-04-23 22:36:33 +0200

    Ez egy kalap szaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaar

---
+++ VXAS5X_HFT_2023241/DesktopFrontend.WpfClient/MainWindowViewModell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VXAS5X_HFT_2023241.Models;
+
+namespace DesktopFrontend.WpfClient
+{
+    public class MainWindowViewModell
+    {
+
+        public RestCollection<Actor> Actors { get; set; }
+        public MainWindowViewModell()
+        {
+            Actors = new RestCollection<Actor>("http://localhost:62255/", "actor");
+        }
+
+    }
+}
